d737af76f80914350b54538f231f46099bfb2214
Harald Sitter <sitter@kde.org>
2014-05-26 13:27:00 +0200

    add completely untested logic for repo publication on NRA

--- config.rb
+++ config.rb
@@ -2,6 +2,7 @@ module Blue
     Config = {
         :pkg_git => 'https://github.com/apachelogger/bluepkg.git',
         :chroot_basepath => '/tmp/chroot/',
-        :build_basepath => '/tmp/'
+        :build_basepath => '/tmp/',
+        :repo_basepath => '/tmp/repo/'
     }
 end
--- targets/netrunnerarch.rb
+++ targets/netrunnerarch.rb
@@ -22,14 +22,74 @@ require 'fileutils'
 
 require_relative 'builder'
 
-class Netrunnerarch < Builder
-    attr_reader :chroot_path
+class Chroot
+    attr_reader :path
+    attr_reader :mounted_paths
+
+    def initialize(path)
+        @path = path
+        @mounted_paths = []
+    end
+
+    def setup()
+        mount('-o bind /proc', '/proc/')
+        mount('-o bind /sys', '/sys/')
+        mount('-o bind /dev', '/dev/')
+        mount('-p', '/dev/pts')
+        mount('-t devpts devpts', '/dev/pts')
+
+        system("cp '/etc/resolv.conf' '#{path}/etc/'") if File.exist?('/etc/resolv.conf')
+
+        # Make sure we overwrite systemd-nspawn to establish an upstart compatible
+        # chroot execution. This is necessary because the manjaro specific build
+        # tools internally will try to nspawn which will fail on systems <=14.04
+        # as there is no systemd available to manage the nspawning.
+        FileUtils.cp("#{File.dirname __FILE__}/netrunnerarch/systemd-nspawn",
+                     "#{path}/usr/local/bin/",
+                     :verbose=>true)
+        FileUtils.cp("#{File.dirname __FILE__}/netrunnerarch/systemd-nspawn",
+                     "#{path}/usr/bin/",
+                     :verbose=>true)
+        FileUtils.cp("#{File.dirname __FILE__}/netrunnerarch/systemd-nspawn",
+                     "#{path}/usr/sbin/",
+                     :verbose=>true)
+
+        # Worksaround a Debian compat issue.
+        FileUtils.mkpath("#{path}/run/shm")
+    end
+
+    def bind(bind_path)
+        mount("-o bind #{bind_path}", bind_path)
+    end
+
+    def run(cmd)
+        return system("chroot #{path} #{cmd}")
+    end
+
+    def teardown()
+        @mounted_paths = mounted_paths.reverse.drop_while do |mounted_path|
+            system("umount #{path}#{mounted_path}")
+        end
+    end
 
 private
-    def setup_chroot()
-        # FIXME: how is this supposed to work with x64 vs x86
+    def mount(options, target_path)
+        puts ":::mount(#{options}, #{target_path})"
+        system("mkdir -p '#{path}#{target_path}'")
+        system("mount #{options} #{path}#{target_path}")
+        @mounted_paths << target_path
+    end
+end
+
+class Netrunnerarch < Builder
+    attr_reader :chroot_path
+    attr_reader :repo_path
+
+private
+# TODO: move to chroot class
+# TODO: what to do with chroot_path, should that maybe simply be set in the Chroot? currently that is NRA exclusive
+    def create_chroot()
         @chroot_path = "#{Blue::Config[:chroot_basepath]}/netrunnerarch"
-        p @chroot_path
         return if File.exist?(chroot_path)
 
         env = {
@@ -47,48 +107,79 @@ private
     end
 
     def build_in_chroot()
-        bind = build_path
+        create_chroot()
 
-        # FIXME: need i686 and x86_64 ... or we somehow get the thing to
-        #        adjust chroots, which seems a bit dirty
-        system("mount -o bind /proc '#{chroot_path}/proc/'")
-        system("mount -o bind /sys '#{chroot_path}/sys/'")
-        system("mount -o bind /dev '#{chroot_path}/dev/'")
-        system("mkdir -p '#{chroot_path}/dev/pts'")
-        system("mount -t devpts devpts '#{chroot_path}/dev/pts/'")
+        Chroot chroot(chroot_path)
+        chroot.setup()
+        chroot.bind(build_path)
 
-        system("cp '/etc/resolv.conf' '#{chroot_path}/etc/'") if File.exist?('/etc/resolv.conf')
-
-        # TODO: need to bind build path I guess, then run stable-build on that path
-        system("mkdir -p '#{chroot_path}/#{bind}'")
-        system("mount -o bind #{bind} '#{chroot_path}/#{bind}'")
-
-        ##
-        # FIXME: needs cmd
-        # TODO: need to copy fake systemd-nspawn
-        FileUtils.cp("#{File.dirname __FILE__}/netrunnerarch/systemd-nspawn", "#{chroot_path}/usr/local/bin/", :verbose=>true)
-        FileUtils.cp("#{File.dirname __FILE__}/netrunnerarch/systemd-nspawn", "#{chroot_path}/usr/bin/", :verbose=>true)
-        FileUtils.cp("#{File.dirname __FILE__}/netrunnerarch/systemd-nspawn", "#{chroot_path}/usr/sbin/", :verbose=>true)
-        p bind
+        # Remove SUDO variables from the envrionment as manjarobuild tries to use it for
+        # chroot creation, making it fail if the user doesn't exist.
         ENV.delete('SUDO_USER')
         ENV.delete('SUDO_UID')
-        system("chroot #{chroot_path}  sh -c 'cd #{bind} && { stable-x86_64-build; stable-i686-build; }'")
+        chroot.run("sh -c 'cd #{build_path} && { stable-x86_64-build; stable-i686-build; }'")
         ##
 
-        system("umount '#{chroot_path}/#{bind}'")
+        chroot.teardown()
+    end
 
-        system("umount '#{chroot_path}/dev/pts'")
-        system("umount '#{chroot_path}/dev'")
-        system("umount '#{chroot_path}/sys'")
-        system("umount '#{chroot_path}/proc'")
+    def repo_add(pkgtar)
     end
 
     def build_internal()
         Dir.chdir(build_path) do
             Dir.glob("#{source.path}/netrunnerarch/*").each do |file|
                 FileUtils.cp_r(file, build_path, :verbose=>true)
-                setup_chroot()
+                create_chroot()
                 build_in_chroot()
+
+                # NOTE: the tar.xs are now supposed to be in build_path
+                pkgarches = []
+                File.readlines('PKGBUILD').each do |line|
+                    if line.start_with?('arch=')
+                        # Sub away the arch=() part.
+                        line.gsub!(/arch=\((.*)\)/, '\1')
+                        # What remains may be a space separate list of ' quoted
+                        # arch identifiers.
+                        # Split by separator, remove the quotes and convert to
+                        # symbol.
+                        line.split(' ').each do |arch|
+                            pkgarches << arch.gsub("'", '').to_sym
+                        end
+                    end
+                end
+
+                pkgtars = Dir.glob('*.pkg.tar.xz')
+
+                # Check that there is a package for all defined architectures.
+                pkgarches.each do |pkgarch|
+                    found = false
+                    pkgtars.each do |pkgtar|
+                        found = true if (pkgarch == File.basename(pkgtar, '.pkg.tar.xz').split('-').last.to_sym)
+                        break if found
+                    end
+                    # No match found, we are missing a build, oh my.
+                    # FIXME: return error etc.
+                    # TODO: should we return at all? maybe publish what we have instead?
+                    puts "netrunnerach pkgbuild for #{pkgarch} failed apparently"
+                    return
+                end
+
+                @repo_path = "#{Blue::Config[:repo_basepath]}/netrunnerarch"
+                FileUtils.mkpath(repo_path)
+                FileUtils.cp(pkgtars, repo_path, :verbose => true)
+                Dir.chdir(repo_path) do
+                    Chroot chroot(chroot_path)
+                    chroot.setup()
+                    chroot.bind(repo_path)
+                    pkgtars.each do |pkgtar|
+                        # TODO: param for repo?
+                        chroot.run("sh -c 'cd #{repo_path} && repo-add bluebuild.db.tar.gz #{pkgtar}'")
+                    end
+                    chroot.teardown()
+                end
+                # TODO: extract logs and whatnot for publication
+                # NOTE: packages are not signed intentionally apparently
             end
         end
     end
